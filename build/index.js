c5b64928d194afddf0248a5ba65ba4e69dc0c457
savva <savva.lambin@gmail.com>
2023-07-20 12:06:06 +0100

    Add CsvFileReader Module

--- build/index.js
+++ build/index.js
@@ -1,2 +1,22 @@
 "use strict";
-console.log('Hi there');
+Object.defineProperty(exports, "__esModule", { value: true });
+const CsvFileReader_1 = require("./CsvFileReader");
+const reader = new CsvFileReader_1.CsvFileReader('football.csv');
+reader.read();
+// enum - enumeration
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
+let arsenalWins = 0;
+for (let match of reader.data) {
+    if (match[1] === 'Arsenal' && match[5] === MatchResult.HomeWin) {
+        arsenalWins++;
+    }
+    else if (match[2] === 'Arsenal' && match[5] === MatchResult.AwayWin) {
+        arsenalWins++;
+    }
+}
+console.log(`Arsenal won ${arsenalWins} games`);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log('Hi there');
+import { CsvFileReader } from './CsvFileReader';
+
+const reader = new CsvFileReader('football.csv');
+reader.read();
+
+// enum - enumeration
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
+
+let arsenalWins = 0;
+
+for (let match of reader.data) {
+  if (match[1] === 'Arsenal' && match[5] === MatchResult.HomeWin) {
+    arsenalWins++;
+  } else if (match[2] === 'Arsenal' && match[5] === MatchResult.AwayWin) {
+    arsenalWins++;
+  }
+}
+
+console.log(`Arsenal won ${arsenalWins} games`);
